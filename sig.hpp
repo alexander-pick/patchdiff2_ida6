e1039b6abf512ad39bf22526ab1e6ca34f7538cb
alexander-pick <ap@pbt-media.com>
2011-03-08 05:15:50 +0100

    first commit

---
+++ sig.hpp
@@ -0,0 +1,183 @@
+/* 
+ Patchdiff2
+ 
+ Portions (C) 2011 Alexander Pick
+ Portions (C) 2010 Nicolas Pouvesle
+ Portions (C) 2007 - 2009 Tenable Network Security, Inc.
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License version 2 as 
+ published by the Free Software Foundation.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef __SIG_H__
+#define __SIG_H__
+
+#include <ida.hpp>
+#include <funcs.hpp>
+#include <gdl.hpp>
+#include <graph.hpp>
+
+#define DIFF_UNMATCHED -1
+
+#define CLASS_SIG 0xACDCACDC
+
+#define SIG_PRED 1
+#define SIG_SUCC 2
+
+#define CHECK_REF 0
+#define DO_NOT_CHECK_REF 1
+
+#ifdef _WINDOWS
+#define OS_CDECL __cdecl
+#else
+#define OS_CDECL
+#endif
+
+typedef struct signature psig_t;
+typedef struct dc_sig *  pdc_sig;
+
+struct dc_sig
+{
+	psig_t * sig;
+	bool removed;
+	pdc_sig prev;
+	pdc_sig next;
+};
+
+typedef struct dc_sig dpsig_t;
+
+struct c_list
+{
+	size_t num;
+	dpsig_t * pos;   // position in sigs list
+	dpsig_t * sigs;  // chained list
+
+	size_t nmatch;	// number of matched element
+	dpsig_t * msigs;	// matched list
+};
+
+typedef struct c_list clist_t;
+
+
+
+typedef struct fct_ref * pfct_ref;
+
+struct fct_ref
+{
+	ea_t ea;
+	int type;
+	char rtype;
+	pfct_ref next;
+};
+
+typedef struct fct_ref fref_t;
+
+struct fct_refs
+{
+	int num;
+	fref_t * list;
+};
+
+typedef struct fct_refs frefs_t;
+
+struct dline
+{
+	int num;
+	int available;
+	char * lines;
+};
+
+typedef struct dline dline_t;
+
+typedef struct signature * psignature;
+
+struct signature
+{
+	char * name;
+	ea_t startEA;
+	ea_t matchedEA;
+	int mtype;
+	psignature msig;
+	int node;
+	int id_crc;
+	int nfile;
+	int type;
+	int flag;
+	unsigned long sig;
+	unsigned long hash;
+	unsigned long hash2;
+	unsigned long crc_hash;
+	unsigned long str_hash;
+	unsigned long lines;
+	frefs_t * prefs;
+	frefs_t * srefs;
+	clist_t * cp;
+	clist_t * cs;
+	dline_t dl;
+};
+
+
+typedef struct sig_list * psig_list;
+
+struct sig_list
+{
+	size_t num;
+	size_t org_num;
+	char * file;
+	bool dclk;
+	graph_viewer_t *gv;
+	bool unique;
+	psig_list msl;
+	psig_t ** sigs;
+};
+
+typedef struct sig_list slist_t;
+
+
+void siglist_free(slist_t *);
+void siglist_partial_free(slist_t *);
+int siglist_save(slist_t *, const char *);
+slist_t * siglist_load(const char *);
+slist_t * siglist_init(size_t, char *);
+psig_t * sig_generate(size_t, qvector<ea_t> &);
+psig_t * sig_class_generate(ea_t);
+void clist_free(clist_t *);
+void siglist_free(slist_t **);
+bool siglist_realloc(slist_t *, size_t);
+void siglist_add(slist_t *, psig_t *);
+void siglist_remove(slist_t *, size_t);
+void siglist_sort(slist_t *);
+ea_t sig_get_start(psig_t * );
+void sig_set_nfile(psig_t *, int);
+void sig_set_matched_sig(psig_t *, psig_t *, int);
+psig_t * sig_get_matched_sig(psig_t *);
+void sig_set_matched_ea(psig_t *, ea_t);
+ea_t sig_get_matched_ea(psig_t *);
+int sig_get_matched_type(psig_t *);
+frefs_t * sig_get_preds(psig_t *);
+frefs_t * sig_get_succs(psig_t *);
+int sig_add_pref(psig_t *, ea_t, int, char);
+int sig_add_sref(psig_t *, ea_t, int, char);
+clist_t * sig_get_crefs(psig_t *, int);
+void sig_set_crefs(psig_t *, int, clist_t *);
+int OS_CDECL sig_compare(const void *, const void *);
+psig_t * sig_init();
+int sig_add_block(psig_t *, short *, ea_t, ea_t, bool, char);
+void sig_set_start(psig_t *, ea_t);
+void sig_set_name(psig_t *, const char *);
+int sig_calc_sighash(psig_t *, short *, int);
+void sig_free(psig_t *);
+bool sig_is_class(psig_t *);
+char * pget_func_name(ea_t, char *, size_t);
+
+#endif
---
+++ unix_fct.cpp
@@ -0,0 +1,306 @@
+/* 
+   Patchdiff2
+ 
+   Portions (C) 2011 Alexander Pick
+   Portions (C) 2010 Nicolas Pouvesle
+   Portions (C) 2007 - 2009 Tenable Network Security, Inc.
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2 as 
+   published by the Free Software Foundation.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#include <ida.hpp>
+#include <kernwin.hpp>
+
+#include "unix_fct.hpp"
+#include "system.hpp"
+
+
+struct ipc_data
+{
+  int spipe;
+  int rpipe;
+  pid_t pid;
+};
+
+typedef struct ipc_data ipc_data_t;
+
+
+/*------------------------------------------------*/
+/* function : create_process                      */
+/* description: fork/exec                         */
+/*------------------------------------------------*/
+
+pid_t create_process(char * cmd)
+{
+  pid_t pid;
+  char * argv[4];
+  char name[512];
+
+  //printf("%s\n", cmd);	
+	
+  pid = fork();
+  if (pid == 0)
+  {
+      qsnprintf(name, sizeof(name), "'%s'", cmd);
+      argv[0] = (char *)"/bin/sh";
+      argv[1] = (char *)"-c";
+      argv[2] = cmd;//(char *)name;
+      argv[3] = NULL;
+		
+	  if(execv(argv[0], argv)) {
+		//printf("error executing IDA");
+	  }
+  }
+
+  //printf("PID: %d\n", pid);	
+	
+  return pid;
+}
+
+
+/*------------------------------------------------*/
+/* function : os_execute_command                  */
+/* description: Executes a command by creating a  */
+/*              new process                       */
+/*------------------------------------------------*/
+
+int os_execute_command(char * cmd, bool close, void * data)
+{
+  ipc_data_t * id = (ipc_data_t *)data;
+  int ret = -1;
+  int status;
+  pid_t pid;
+
+  pid = create_process(cmd);
+  if (pid == -1) return -1;
+
+  //printf("%s\n", cmd);	
+  //printf("PID: %d\n", pid);
+	
+  if (close){
+	  /* we wait until the process finished (IE: sig file is generated) */
+	  if (waitpid(pid, &status, 0) == -1) {
+		  return -1;
+	  }
+  }  else {
+    id->pid = pid;
+  }
+	  
+  return 0;
+}
+
+
+/*------------------------------------------------*/
+/* function : os_check_process                    */
+/* description: checks process state              */
+/*------------------------------------------------*/
+
+bool os_check_process(pid_t pid)
+{
+  //waitpid(pid, status, WNOHANG)
+
+  //printf("process state\n");	
+  if (kill(pid, 0) == 0)
+    return true;
+  
+  return false;
+}
+
+
+/*------------------------------------------------*/
+/* function : os_copy_to_clipboard                */
+/* description: Copies data to clipboard          */
+/*------------------------------------------------*/
+
+void os_copy_to_clipboard(char * data)
+{
+}
+
+
+/*------------------------------------------------*/
+/* function : os_get_pid                          */
+/* description: Returns process ID                */
+/*------------------------------------------------*/
+
+long os_get_pid()
+{
+  return (long)getpid();
+}
+
+
+/*------------------------------------------------*/
+/* function : os_unlink                           */
+/* description: removes a link to a file          */
+/*------------------------------------------------*/
+
+int os_unlink(const char * path)
+{
+  return unlink(path);
+}
+
+
+/*------------------------------------------------*/
+/* function : os_ipc_send                         */
+/* description: Sends data on pipe                */
+/*------------------------------------------------*/
+
+bool os_ipc_send(void * data, int type, idata_t * d)
+{
+  ipc_data_t * id = (ipc_data_t *)data;
+  ssize_t num;
+	
+  num = write(id->spipe, d, sizeof(*d));
+  if (num > 0) return true;
+
+  return false;
+}
+
+
+/*------------------------------------------------*/
+/* function : os_ipc_recv                         */
+/* description: Receives data on pipe             */
+/*------------------------------------------------*/
+
+bool os_ipc_recv(void * data, int type, idata_t * d)
+{
+  ipc_data_t * id = (ipc_data_t *)data;
+  fd_set rfds, efds;
+  struct timeval tv;
+  int ret;
+  
+  tv.tv_sec = 0;
+  tv.tv_usec = 1000;
+	
+  if (id->pid)
+  {
+	  while (1)
+	  {
+		  FD_ZERO(&rfds);
+		  FD_SET(id->rpipe, &rfds);
+
+		  FD_ZERO(&efds);
+		  FD_SET(id->rpipe, &efds);
+		
+		  ret = select(1, &rfds, NULL, &efds, &tv);
+	  
+		  if (ret > 0 && FD_ISSET(id->rpipe, &rfds)) {
+			  break;
+		  }
+			
+		  if (ret < 0 || (ret > 0 && FD_ISSET(id->rpipe, &efds)) || !os_check_process(id->pid)) {
+			  return false;
+		  }
+	  }
+  } else {
+      FD_ZERO(&rfds);
+      FD_SET(id->rpipe, &rfds);
+
+      FD_ZERO(&efds);
+      FD_SET(id->rpipe, &efds);
+
+      ret = select(1, &rfds, NULL, &efds, NULL);
+	  if (!(ret > 0 && FD_ISSET(id->rpipe, &rfds))) {
+		  return false;
+	  }
+  }
+ 	
+  read(id->rpipe, d, sizeof(*d));
+  
+  return true;
+}
+
+
+/*------------------------------------------------*/
+/* function : os_ipc_init                         */
+/* description: Inits interprocess communication  */
+/*------------------------------------------------*/
+
+bool os_ipc_init(void ** data, long pid, int type)
+{
+  char sname[512];
+  char rname[512];
+  ipc_data_t * id;
+
+  id = (ipc_data_t *)qalloc(sizeof(*id));
+  if (!id)
+    return false;
+  
+  memset(id, '\0', sizeof(*id));
+	
+  qsnprintf(sname, sizeof(sname), "/tmp/pdiff2spipe%ld", pid);
+  qsnprintf(rname, sizeof(rname), "/tmp/pdiff2rpipe%ld", pid);
+  
+  if (type == IPC_SERVER)
+    {
+      mkfifo(sname, 0666);
+      id->spipe = open(sname, O_RDWR|O_NONBLOCK);
+      if (id->spipe == -1) goto error;
+
+      mkfifo(rname, 0666);
+      id->rpipe = open(rname, O_RDWR|O_NONBLOCK);
+      if (id->rpipe == -1) goto error;
+    }
+  else
+    {
+      id->spipe = open(rname, O_WRONLY);
+      if (id->spipe == -1) goto error;
+
+      id->rpipe = open(sname, O_RDONLY);
+      if (id->rpipe == -1) goto error;
+    }
+
+  *data = (void *)id;
+  return true;
+
+error:
+	
+  //printf("IPC Error occured\n");	
+  if (id->spipe) close(id->spipe);
+  if (id->rpipe) close(id->rpipe);
+  qfree(id);
+  return false;
+}
+
+
+/*------------------------------------------------*/
+/* function : os_ipc_close                        */
+/* description: Closes IPC                        */
+/*------------------------------------------------*/
+
+bool os_ipc_close(void * data)
+{
+  ipc_data_t * id = (ipc_data_t *)data;
+
+  if (id->spipe) close(id->spipe);
+  if (id->rpipe) close(id->rpipe);
+
+  /* TODO: unlink pipe */
+  return true;
+}
+
+
+/*------------------------------------------------*/
+/* function : os_get_pref_int                     */
+/* description: Gets system preferences (integer) */
+/*------------------------------------------------*/
+
+bool os_get_pref_int(char * name, int * i)
+{
+  *i = 0;
+  return false;
+}
